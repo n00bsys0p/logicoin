03f965b54e1640023fbdd880830da4bb15842cac
Alex Shepherd <n00b@n00bsys0p.co.uk>
2014-04-22 21:50:00 +0100

    Changed RPC port from Darkcoin's (which this is a fork of)

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 19998 : 9998;
+    return GetBoolArg("-testnet", false) ? 19652 : 9652;
 }
 
 Object JSONRPCError(int code, const string& message)
